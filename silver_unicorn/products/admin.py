22ecf0493db4615274b01802173fe086e24403d2
Alfrederick888 <al-gilm@yandex.ru>
2023-05-20 11:59:46 +0500

    Initial commit

---
+++ silver_unicorn/products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Products)
+admin.site.register(Status)
+admin.site.register(Orders)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'size', 'picture', 'price')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_editable = ('is_published',)
